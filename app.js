105b39d3b159514d4885192a5b74ef4a34cf1c63
yefwal <145699390+yefwal@users.noreply.github.com>
2023-10-24 09:26:57 -0700

    - completed - did the random color extra credit - also turns out I need to get better at "talking to the duck"

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-const grid = document.querySelector("#grid");
-let isDrawing = false;
-
 const sliderValue = document.querySelector("input");
-sliderOutput.textContent = 16;
-sliderValue.value = 16;
+sliderOutput.textContent = "32 x 32";
+sliderValue.value = 32;
 
 sliderValue.addEventListener("input", (e) => {
-  sliderOutput.textContent = e.target.value;
+  sliderOutput.textContent = `${e.target.value} x ${e.target.value}`;
   createGrid(e.target.value);
+});
+
+const grid = document.querySelector("#grid");
+let isDrawing = false;
+let useRandom = false;
+
+const random = document.querySelector("#random");
+
+randomColor = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  let final = `rgb(${r}, ${g}, ${b})`;
+
+  return final;
+};
+
+random.addEventListener("click", () => {
+  useRandom = !useRandom;
+  console.log("Random flag:", useRandom);
 });
 
 changeColor = (e) => {
   if (isDrawing) {
-    e.target.style.backgroundColor = "black";
+    if (useRandom) {
+      e.target.style.backgroundColor = randomColor();
+    } else {
+      e.target.style.backgroundColor = "black";
+    }
   }
 };
 
@@ -23,19 +45,37 @@ createGrid = (size) => {
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement("div");
+
     gridElement.addEventListener("click", (e) => {
-      e.target.style.backgroundColor = "black";
+      if (useRandom) {
+        e.target.style.backgroundColor = randomColor();
+      } else {
+        e.target.style.backgroundColor = "black";
+      }
     });
+
+    gridElement.addEventListener("mouseenter", (e) => {
+      if (isDrawing) {
+        if (useRandom) {
+          e.target.style.backgroundColor = randomColor();
+        } else {
+          e.target.style.backgroundColor = "black";
+        }
+      }
+    });
+
     gridElement.addEventListener("mousedown", () => {
       isDrawing = true;
     });
+
     gridElement.addEventListener("mouseup", () => {
       isDrawing = false;
     });
+
     grid.addEventListener("mouseleave", (e) => {
       isDrawing = false;
     });
-    gridElement.addEventListener("mouseenter", changeColor);
+
     gridElement.classList.add("gridElement");
     grid.append(gridElement);
   }
